ac2b834f05f2e143571d0bae0b5a44a042787fc0
mr-mojtaba <mr.mojtaba.mailgmail.com>
2024-01-07 13:44:44 +0330

    Creating some URLs and creating view them.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def index(request):
+    return HttpResponse('Index')
+
+
+def post_list(request):
+    return HttpResponse('Post list')
+
+
+def post_detail(request, id):
+    return HttpResponse(f'Post:{id}')
