9d0f8844923c785298323b15d4638e71174492ac
mr-mojtaba <mr.mojtaba.mail@gmail.com>
2024-01-28 15:26:06 +0330

    Adding choice option for the subject field

--- newshub/blog/forms.py
+++ newshub/blog/forms.py
@@ -2,8 +2,14 @@ from django import forms
 
 
 class TicketForm(forms.Form):
+    SUBJECT_CHOICES = (
+        ('پیشنهاد', 'پیشنهاد'),
+        ('انتقاد', 'انتقاد'),
+        ('گزارش', 'گزارش'),
+    )
+
     message = forms.CharField(widget=forms.Textarea, required=True)
     name = forms.CharField(max_length=250, required=True)
     email = forms.EmailField()
     phone = forms.CharField(max_length=11, required=True)
-    subject = forms.CharField(max_length=250)
+    subject = forms.ChoiceField(choices=SUBJECT_CHOICES)
