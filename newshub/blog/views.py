dd32cc065b5d37569a8f960cd7376a047cbc4b48
mr-mojtaba <mr.mojtaba.mail@gmail.com>
2024-01-28 12:56:09 +0330

    Adding PostDetailView class in the views.py

--- newshub/blog/views.py
+++ newshub/blog/views.py
@@ -2,33 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 
 # Create your views here.
 def index(request):
     return HttpResponse('Index')
-
-
-def post_list(request):
-    posts = Post.published.all()
-
-    # # Creating an object of the paginator class.
-    paginator = Paginator(posts, 3)
-    # Specifying the page number.
-    page_number = request.GET.get('page', 1)
-    # Resetting the value with page_number.
-    try:
-        posts = paginator.page(page_number)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-
-    context = {
-        'posts': posts,
-    }
-    return render(request, 'blog/list.html', context)
 
 
 def post_detail(request, id):
@@ -40,7 +19,16 @@ def post_detail(request, id):
 
 
 class PostListView(ListView):
+    # Returns published posts.
+    queryset = Post.published.all()
+
+    # Defining name for the object.
+    context_object_name = "posts"
+
     paginate_by = 3
     template_name = "blog/list.html"
-    queryset = Post.published.all()
-    context_object_name = "posts"
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/detail.html"
