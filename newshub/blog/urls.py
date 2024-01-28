bb3eb465cdbcdb290fe5fd7ba00b5b2ea3162e31
mr-mojtaba <mr.mojtaba.mail@gmail.com>
2024-01-28 12:45:20 +0330

    Changing the url path

--- newshub/blog/urls.py
+++ newshub/blog/urls.py
@@ -5,6 +5,6 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('posts/', views.post_list, name='post_list'),
-    path('posts/<int:id>', views.post_detail, name='post_detail')
+    path('posts/', views.PostListView.as_view(), name='post_list'),
+    path('posts/<int:id>', views.post_detail, name='post_detail'),
 ]
