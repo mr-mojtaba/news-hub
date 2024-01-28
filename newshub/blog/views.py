de1a01cb67576cae6db2f27e6faba98126f8ef27
mr-mojtaba <mr.mojtaba.mail@gmail.com>
2024-01-28 15:22:44 +0330

    Creating the ticket function in the views.py

--- newshub/blog/views.py
+++ newshub/blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, Http404
-from .models import Post
+from .models import *
+from .forms import *
 from django.views.generic import ListView, DetailView
 
 
@@ -23,3 +24,36 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
     template_name = "blog/detail.html"
+
+
+def ticket(request):
+    if request.method == "POST":
+        # Creating a Variable from the TicketForm.
+        form = TicketForm(request.POST)
+
+        # Form data validation.
+        if form.is_valid():
+            # Creating an object from the Ticket model.
+            ticket_obj = Ticket.objects.create()
+
+            # Creating a variable and assigning it with form values.
+            # (cleaned_data is a dictionary).
+            cd = form.cleaned_data
+
+            #  Initialization of each Ticket field with the values of each Ticket Form field.
+            ticket_obj.message = cd['message']
+            ticket_obj.name = cd['name']
+            ticket_obj.email = cd['email']
+            ticket_obj.phone = cd['phone']
+            ticket_obj.subject = cd['subject']
+
+            # Storage in the database.
+            ticket_obj.save()
+
+            return redirect('blog:ticket')
+    else:
+        # Creating a variable from TicketForm with no value.
+        form = TicketForm()
+
+    # Show ticket.html page.
+    return render(request, 'forms/ticket.html', {'form': form})
