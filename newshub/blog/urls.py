99edd8c684f078ffe07ba9c7be81576605f3f35c
mr-mojtaba <mr.mojtaba.mail@gmail.com>
2024-01-31 21:03:45 +0330

    Adding the comment url

--- newshub/blog/urls.py
+++ newshub/blog/urls.py
@@ -6,6 +6,7 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
     path('posts/', views.PostListView.as_view(), name='post_list'),
-    path('posts/<pk>', views.PostDetailView.as_view(), name='post_detail'),
+    path('posts/<int:id>', views.post_detail, name='post_detail'),
     path('ticket', views.ticket, name='ticket'),
+    path('posts/<int:post_id>/comment', views.post_comment, name='post_comment'),
 ]
