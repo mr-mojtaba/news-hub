0d8bf7d17d442432a06af39a12e552142cd1b3fd
mr-mojtaba <mr.mojtaba.mail@gmail.com>
2024-01-31 20:58:27 +0330

    Changing the PostDetailView class to the post_detail function

--- newshub/blog/views.py
+++ newshub/blog/views.py
@@ -22,9 +22,30 @@ class PostListView(ListView):
     template_name = "blog/list.html"
 
 
-class PostDetailView(DetailView):
-    model = Post
-    template_name = "blog/detail.html"
+def post_detail(request, id):
+    post = get_object_or_404(
+        Post,
+        id=id,
+        status=Post.Status.PUBLISHED
+    )
+
+    # Creating a variable and put the approved comments in it.
+    comments = post.comments.filter(active=True)
+
+    # Creating an empty form.
+    form = CommentForm()
+
+    context = {
+        'post': post,
+        'form': form,
+        'comments': comments,
+    }
+
+    return render(
+        request,
+        "blog/detail.html",
+        context
+    )
 
 
 def ticket(request):
