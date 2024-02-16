5e9c799b3479fdf73a556e4a4a86e1fc9031bba1
mr-mojtaba <mr.mojtaba.mail@gmail.com>
2024-02-16 19:14:43 +0330

    Adding the create_post url.

--- newshub/blog/urls.py
+++ newshub/blog/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('posts/<int:id>', views.post_detail, name='post_detail'),
     path('ticket', views.ticket, name='ticket'),
     path('posts/<int:post_id>/comment', views.post_comment, name='post_comment'),
+    path('create_post/', views.create_post, name='create_post'),
 ]
