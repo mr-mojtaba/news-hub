47907f60bd938e8e2fce89dc4b2d1ec6e51af7ea
mr-mojtaba <mr.mojtaba.mail@gmail.com>
2024-02-18 20:35:30 +0330

    Changing the sqlite database to the postgresql database.

--- newshub/newshub/settings.py
+++ newshub/newshub/settings.py
@@ -75,10 +75,20 @@ WSGI_APPLICATION = 'newshub.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'newshub_blog',
+        'USER': 'newshub_blog_admin',
+        'PASSWORD': '13745123',
+        'PORT': '5432',
     }
 }
 
