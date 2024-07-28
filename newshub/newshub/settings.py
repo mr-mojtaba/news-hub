16b243a4dd0d4bed8e0c84abab05697963049e79
mr-mojtaba <mr.mojtaba.mail@gmail.com>
2024-07-28 15:13:23 +0330

    Changing the database to the sqlite3.

--- newshub/newshub/settings.py
+++ newshub/newshub/settings.py
@@ -75,22 +75,22 @@ WSGI_APPLICATION = 'newshub.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'newshub_blog',
-        'USER': 'newshub_blog_admin',
-        'PASSWORD': '13745123',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'newshub_blog',
+#         'USER': 'newshub_blog_admin',
+#         'PASSWORD': '13745123',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
