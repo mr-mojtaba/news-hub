e3e7de34e035204808d6b53855c3bf97359781ed
mr-mojtaba <mr.mojtaba.mail@gmail.com>
2024-08-14 15:51:26 +0330

    create post search url

--- newshub/blog/urls.py
+++ newshub/blog/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('ticket', views.ticket, name='ticket'),
     path('posts/<int:post_id>/comment', views.post_comment, name='post_comment'),
     path('create_post/', views.create_post, name='create_post'),
+    path('search/', views.post_search, name='post_search'),
 ]
