4b5baa937916449ab2be67413aa4edec1791267a
mr-mojtaba <mr.mojtaba.mail@gmail.com>
2024-08-28 18:43:28 +0330

    Completion of comments for codes.

--- newshub/blog/admin.py
+++ newshub/blog/admin.py
@@ -1,29 +1,30 @@
 from django.contrib import admin
 
-# For customization of the admin panel
+# Import models and necessary packages for customization of the admin panel.
 from .models import *
 
-# Need to install ( pip install django_jalali )
+# Need to install ( pip install django_jalali ) for using Jalali date filters.
 from django_jalali.admin.filters import JDateFieldListFilter
 import django_jalali.admin as jadmin
 
+# Customization of the Django admin site headers and titles
 admin.sites.AdminSite.site_header = "پنل مدیریت جنگو"
 admin.sites.AdminSite.site_title = "پنل"
 admin.sites.AdminSite.index_title = "پنل مدیریت"
 
 
-# Register your models here.
-# Customization of the admin panel.
-
-#Inlines
+# Define inlines for related models to be displayed in the parent model’s admin page.
 class ImageInline(admin.StackedInline):
     model = Image
+    # No extra empty forms by default
     extra = 0
 
 
 class CommentInline(admin.TabularInline):
     model = Comment
+    # No extra empty forms by default
     extra = 0
+    # Makes these fields read-only
     readonly_fields = (
         'name',
         'body',
@@ -31,47 +32,51 @@ class CommentInline(admin.TabularInline):
     )
 
 
+# Customize the Post model admin interface.
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    # Showing these fields in the top bar of the posts display.
+    # Fields to be displayed in the list view of posts in the admin panel.
     list_display = ['title', 'author', 'publish', 'status']
 
-    # Defining the default list for sorting fields.
+    # Default sorting order.
     ordering = ['title', 'author']
 
-    # Creating a search bar and a list of searchable fields.
+    # Add a search bar with these fields as searchable.
     search_fields = ['title', 'description']
 
-    # Creating a filter page and a list of filterable fields.
+    # Filters to allow admin to narrow down the list of posts.
     list_filter = ['status', 'author', ('publish', JDateFieldListFilter)]
 
-    # Assigning an ID to each author and
-    # changing the type of author editing in the post manager.
+    # Allow admins to pick authors from a list of IDs.
     raw_id_fields = ['author']
 
-    # Creating a time hierarchy filter based on a field.
+    # Enables navigation by date hierarchy.
     date_hierarchy = 'publish'
 
-    # To automatically complete the slug field based on title.
+    # Automatically populates the slug field based on the title.
     prepopulated_fields = {'slug': ['title']}
 
-    # Creating the ability to edit each field in the table.
+    # Allows editing the status directly from the list view.
     list_editable = ['status']
 
-    # Converting each field to a link for opening the post manager page.
+    # These fields will be clickable to view the detail page.
     list_display_links = ['title', 'author', 'publish']
 
+    # Adds inlines for related images and comments.
     inlines = [ImageInline, CommentInline]
 
 
+# Customize the Ticket model admin interface.
 @admin.register(Ticket)
 class TicketAdmin(admin.ModelAdmin):
+    # Fields to be displayed in the list view of tickets in the admin panel.
     list_display = ['name', 'subject', 'phone']
 
 
+# Customize the Comment model admin interface.
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    # Showing these fields in the top bar of the posts display.
+    # Fields to be displayed in the list view of comments in the admin panel.
     list_display = [
         'post',
         'name',
@@ -79,27 +84,29 @@ class CommentAdmin(admin.ModelAdmin):
         'active',
     ]
 
-    # Creating a filter page and a list of filterable fields.
+    # Filters to allow admin to narrow down the list of comments.
     list_filter = [
         'active',
         ('created', JDateFieldListFilter),
         ('updated', JDateFieldListFilter),
     ]
 
-    # Creating a search bar and a list of searchable fields.
+    # Add a search bar with these fields as searchable.
     search_fields = [
         'name',
         'body',
     ]
 
-    # Creating the ability to edit each field in the table.
+    # Allows editing the active field directly from the list view.
     list_editable = [
         'active'
     ]
 
 
+# Customize the Image model admin interface.
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
+    # Fields to be displayed in the list view of images in the admin panel.
     list_display = [
         'post',
         'title',
