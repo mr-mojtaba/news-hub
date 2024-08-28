d99423f7f092cc12f81831b9eca8dfa2d48f3320
mr-mojtaba <mr.mojtaba.mail@gmail.com>
2024-08-28 19:14:35 +0330

    Completion of comments for codes.

--- newshub/blog/apps.py
+++ newshub/blog/apps.py
@@ -1,7 +1,13 @@
 from django.apps import AppConfig
 
 
+# This class configures the 'blog' application.
 class BlogConfig(AppConfig):
+    # Sets the default auto field type for models in this application.
     default_auto_field = 'django.db.models.BigAutoField'
+
+    # The name of the application used in Django settings.
     name = 'blog'
+
+    # A human-readable name for the application that appears in the Django admin panel.
     verbose_name = "وبلاگ"
